382a7f93049f388ec9c75d6aba2b588cfff9fbb9
sajdkabir <sajda.kbir@gmail.com>
2025-03-13 23:33:27 +0530

    feat: web and sage

---
+++ apps/web/src/utils/constants/cookie.ts
@@ -0,0 +1,19 @@
+/**
+ * The max age of the cookie in seconds
+ */
+export const MAX_AGE = 60 * 60 * 24 * 29; // 29 Days
+
+/**
+ * The name of the cookie
+ */
+export const COOKIE_NAME = "petproject";
+
+/**
+ * The name of the access token cookie
+ */
+export const ACCESS_TOKEN = "petprojectAccess";
+
+/**
+ * The name of the refresh token cookie
+ */
+export const REFRESH_TOKEN = "petprojectRefresh";
